e79572fd7b7ec1b78f80068424d522005263de20
BRENT RITCHIE <brent.ritchie@cambriancollege.ca>
2020-10-29 00:06:17 -0400

    Setup initial configuration

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+if (process.env.NODE_ENV === "development") {
+    require("dotenv").config();
+}
+
+module.exports = function (eleventyConfig) {
+    eleventyConfig.setDataDeepMerge(true);
+
+    return {
+        dir: {
+          input: "_src",
+          output: "_site",
+          includes: "_includes",
+          data: "_data"
+        },
+        templateFormats: ["html", "njk", "11ty.js"]
+      };
+}

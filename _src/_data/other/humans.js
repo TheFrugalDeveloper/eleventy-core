113db4b3d6379b957ae91429cc54bfce6a90cf63
BRENT RITCHIE <brent.ritchie@cambriancollege.ca>
2020-11-10 21:10:36 -0500

    add humans.txt

---
+++ _src/_data/other/humans.js
@@ -0,0 +1,16 @@
+module.exports = {
+    team: {
+        "Eleventy Core Developer": "Brent Ritchie",
+        "twitter": "@thefrugaldeveloper",
+        "website": "thefrugaldeveloper.life",
+        "email": "brent@thefrugaldeveloper.life"
+    },
+    thanks: [
+        "Zach Leatherman (@zachleat)"
+    ],
+    site: {
+        Standards: "CSS, HTML, robots.txt, humans.txt, SVG, Sitemap, opensearch, NPM, node",
+        Software: "Eleventy, VS Code, Eleventy Core"
+    },
+    note: "Eleventy Core is made in Canada!"
+}

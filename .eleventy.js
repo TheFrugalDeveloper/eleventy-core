6f8c83f12889510be5cdaefb3a56b2d95cb5da1d
BRENT RITCHIE <brent.ritchie@cambriancollege.ca>
2020-11-11 22:13:33 -0500

    Fix spacing

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,17 @@
 if (process.env.NODE_ENV === "development") {
-    require("dotenv").config();
+  require("dotenv").config();
 }
 
 module.exports = function (eleventyConfig) {
-    eleventyConfig.setDataDeepMerge(true);
+  eleventyConfig.setDataDeepMerge(true);
 
-    return {
-        dir: {
-          input: "_src",
-          output: "_site",
-          includes: "_includes",
-          data: "_data"
-        },
+  return {
+    dir: {
+      input: "_src",
+      output: "_site",
+      includes: "_includes",
+      data: "_data"
+    },
     templateFormats: ["html", "njk", "md", "11ty.js"]
-      };
+  };
 }
